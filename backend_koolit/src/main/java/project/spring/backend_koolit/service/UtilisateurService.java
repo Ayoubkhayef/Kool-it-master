e2c1000db8a91f2948fc8916b81a21f113fcd49f
Ayoub <ayoub.khayef.allah@gmail.com>
2023-11-27 19:39:49 +0100

    test

---
+++ backend_koolit/src/main/java/project/spring/backend_koolit/service/UtilisateurService.java
@@ -0,0 +1,7 @@
+package project.spring.backend_koolit.service;
+
+public interface UtilisateurService {
+    // ... autres méthodes
+
+    void ajouterListeCourse(Utilisateur utilisateur, ListeCourse listeCourse);
+}
